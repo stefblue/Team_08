9de976f150d020ed3ac910cc3210bc8bb9e98850
Johannes Lederer <johannes_lederer@aon.at>
2021-04-21 08:28:02 +0200

    AC-02 add missing files [LJ, FL]

--- AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/ExampleInstrumentedTest.kt
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/ExampleInstrumentedTest.kt
@@ -17,7 +17,7 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+        // Context of the app under test. Hallo
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.swt.augmentmycampus", appContext.packageName)
     }
