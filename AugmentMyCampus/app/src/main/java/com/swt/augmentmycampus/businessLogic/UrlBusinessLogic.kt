a98e6b8437ccc563bcb8f0a8308dd51723cd382e
Laurenz Fussenegger <laurenz.fussenegger@gmx.at>
2021-04-25 17:46:25 +0200

    AC-02 Implement logic of valid url check [LJ, FL]

--- AugmentMyCampus/app/src/main/java/com/swt/augmentmycampus/businessLogic/UrlBusinessLogic.kt
+++ AugmentMyCampus/app/src/main/java/com/swt/augmentmycampus/businessLogic/UrlBusinessLogic.kt
@@ -1,11 +1,23 @@
 package com.swt.augmentmycampus.businessLogic
 
+import android.util.Patterns
+import android.webkit.URLUtil
+import androidx.core.util.PatternsCompat
+import java.net.MalformedURLException
+import java.net.URL
+
 interface UrlBusinessLogic {
     fun isValidUrlFormat(url: String): Boolean
 }
 
 class UrlBusinessLogicImpl : UrlBusinessLogic {
     override fun isValidUrlFormat(url: String): Boolean {
-        throw NotImplementedError()
+        try {
+            URL(url)
+            return PatternsCompat.WEB_URL.matcher(url).matches()
+        } catch (ex: Exception) {
+            // Do nothing
+        }
+        return false
     }
 }
