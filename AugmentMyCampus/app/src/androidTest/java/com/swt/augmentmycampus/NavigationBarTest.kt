448bd0692fe2520c9e5e1ea3ddaa25bb8163c417
Wachter Florian <florian.wachter@student.tugraz.at>
2021-04-28 14:30:48 +0200

    AC-15 added implemtation of nav bar, fixed a syntax error in test and added icon [HL,WF]

--- AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/NavigationBarTest.kt
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/NavigationBarTest.kt
@@ -1,5 +1,6 @@
 package com.swt.augmentmycampus
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -39,7 +40,7 @@ class NavigationBarTest {
     fun testExistence() {
         mainActivity
         // The whole bar itself
-        onView(withId(R.id.toolbar)).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
 
         // Each sub-option of the bar
         onView(withId(R.id.navigation_camera)).check(matches(isDisplayed()))
@@ -50,95 +51,45 @@ class NavigationBarTest {
     @Test
     fun testAbilityToSwitchBetweenFragments() {
         mainActivity
-
         // Check initial camera fragment
-        onView(withId(R.layout.fragment_camera)).check(matches(isDisplayed()))
         onView(withId(R.id.navigation_camera)).perform(click())
-        onView(withId(R.layout.fragment_camera)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_camera_id)).check(matches(isDisplayed()))
+        onView(withId(R.id.navigation_camera)).perform(click())
+        onView(withId(R.id.fragment_camera_id)).check(matches(isDisplayed()))
 
         // Check camera -> data -> camera
         onView(withId(R.id.navigation_data)).perform(click())
-        onView(withId(R.layout.fragment_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_data_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_data)).perform(click())
-        onView(withId(R.layout.fragment_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_data_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_camera)).perform(click())
-        onView(withId(R.layout.fragment_camera)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_camera_id)).check(matches(isDisplayed()))
 
 
         // Check camera -> settings -> camera
         onView(withId(R.id.navigation_settings)).perform(click())
-        onView(withId(R.layout.fragment_settings)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_settings_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_settings)).perform(click())
-        onView(withId(R.layout.fragment_settings)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_settings_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_camera)).perform(click())
-        onView(withId(R.layout.fragment_camera)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_camera_id)).check(matches(isDisplayed()))
 
 
         // Check data -> settings -> data (-> camera)
         onView(withId(R.id.navigation_data)).perform(click())
-        onView(withId(R.layout.fragment_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_data_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_settings)).perform(click())
-        onView(withId(R.layout.fragment_settings)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_settings_id)).check(matches(isDisplayed()))
 
         onView(withId(R.id.navigation_data)).perform(click())
-        onView(withId(R.layout.fragment_data)).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_data_id)).check(matches(isDisplayed()))
         // we'll switch back to camera to be in a defined state
         onView(withId(R.id.navigation_camera)).perform(click())
-        onView(withId(R.layout.fragment_camera)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun testMainActivity() {
-        mainActivity
-        onView(withId(R.id.scanButton)).check(matches(isDisplayed()))
-        onView(withId(R.id.scanButton)).check(matches(withText("Scan")))
-        onView(withId(R.id.qrCodeText)).check(matches(isDisplayed()))
-        onView(withId(R.id.qrCodeText)).check(matches(withText("Press 'Scan' below")))
-    }
-
-    @Test
-    fun testScannerView() {
-        mainActivity
-        onView(withId(R.id.scanButton)).perform(click())
-        onView(withClassName(Matchers.containsString("ZXingScannerView"))).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun testCameraView() {
-        mainActivity
-        onView(withId(R.id.qrCodeText)).check(matches(isDisplayed()))
-        onView(withText("Press 'Scan' below")).check(matches(isDisplayed()))
-        onView(withId(R.id.scanButton)).perform(click())
-        onView(withClassName(Matchers.containsString("ZXingScannerView")))
-
-        // Wait for user to scan qr-code (5000ms)
-        // result can be either user scanned qr.code
-        // or the timeout
-        // both return to mainactivity
-        Thread.sleep(5500);
-
-        onView(withClassName(Matchers.containsString("ZXingScannerView"))).check(doesNotExist())
-        onView(withText("Press 'Scan' below")).check(doesNotExist())
-    }
-
-    @Test
-    fun testBackButtonOnCameraView() {
-        mainActivity
-        onView(withId(R.id.qrCodeText)).check(matches(isDisplayed()))
-        onView(withText("Press 'Scan' below")).check(matches(isDisplayed()))
-        onView(withId(R.id.scanButton)).perform(click())
-        onView(withClassName(Matchers.containsString("ZXingScannerView")))
-
-        // Wait for button to be available (500ms)
-        Thread.sleep(1000);
-        onView(withId(R.id.backButton)).perform(click())
-
-        onView(withClassName(Matchers.containsString("ZXingScannerView"))).check(doesNotExist())
-        onView(withText("Press 'Scan' below")).check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_camera_id)).check(matches(isDisplayed()))
     }
 }
