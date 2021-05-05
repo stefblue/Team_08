e21d8f3ec536258e1bf6024f4697ea4f28a6d1cd
Alex <alexander.sartory@gmail.com>
2021-05-05 09:51:31 +0200

    AC-12.2 navigate to datafragment test [SA,WF]

---
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/ui/DataFragmentTest.kt
@@ -0,0 +1,33 @@
+package com.swt.augmentmycampus.ui
+
+import androidx.fragment.app.testing.FragmentScenario
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.swt.augmentmycampus.MainActivity
+import com.swt.augmentmycampus.R
+import com.swt.augmentmycampus.ScanActivity
+import com.swt.augmentmycampus.ui.data.DataFragment
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DataFragmentTest {
+
+    @get:Rule
+    var mainActivity: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testNavigateToDataFragment() {
+        onView(withId(R.id.navigation_data)).perform(click())
+        onView(withId(R.id.fragment_data_id)).check(matches(isDisplayed()))
+    }
+
+}
