d0eea0af82f2a4b66d7c51da150637aca6ec2d98
Alex <alexander.sartory@gmail.com>
2021-05-05 10:38:01 +0200

    AC-12.2 header-label implementation [SA,WF]

--- AugmentMyCampus/app/src/main/java/com/swt/augmentmycampus/ui/data/DataViewModel.kt
+++ AugmentMyCampus/app/src/main/java/com/swt/augmentmycampus/ui/data/DataViewModel.kt
@@ -7,7 +7,6 @@ import androidx.lifecycle.ViewModel
 class DataViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is data Fragment"
     }
     val text: LiveData<String> = _text
 }
