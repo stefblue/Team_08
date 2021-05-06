9cb3ce160a11bf8a7f270c879c04c1d9b06de624
Laurenz Fussenegger <laurenz.fussenegger@gmx.at>
2021-05-06 14:29:13 +0200

    AC-18 implement choosing of language [FL,PN]

--- AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/SettingsTest.kt
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/SettingsTest.kt
@@ -1,18 +1,21 @@
 package com.swt.augmentmycampus
 
-import androidx.test.espresso.Espresso.closeSoftKeyboard
-import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class SettingsTest {
 
     @get:Rule
