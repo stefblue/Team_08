440edec9abc87795991456ddd66ecd4e3a75002b
Stefanie Ziegler <ziegler@student.tugraz.at>
2021-05-22 15:14:55 +0200

    AC-12.3 test for import into data fragment & ects fragment [SA, ZS]

---
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/ui/FillDataFragmentTest.kt
@@ -0,0 +1,57 @@
+package com.swt.augmentmycampus.ui
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.support.test.InstrumentationRegistry
+import androidx.core.os.bundleOf
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.NavDeepLinkBuilder
+import androidx.navigation.NavDirections
+import androidx.navigation.findNavController
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jakewharton.espresso.OkHttp3IdlingResource
+import com.swt.augmentmycampus.MainActivity
+import com.swt.augmentmycampus.R
+import com.swt.augmentmycampus.businessLogic.DataBusinessLogicImpl
+import com.swt.augmentmycampus.dependencyInjection.ApplicationModule
+import com.swt.augmentmycampus.dependencyInjection.WebserviceConfiguration
+import com.swt.augmentmycampus.ui.data.DataFragment
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+class FillDataFragmentTest {
+
+    @get:Rule(order = 0)
+    var hiltRule: HiltAndroidRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    var mainActivity: ActivityScenarioRule<MainActivity>
+            = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testECTSField() {
+        mainActivity.scenario.onActivity { a ->
+            a.findNavController(R.id.nav_host_fragment).navigate(R.id.action_navigation_camera_to_navigation_data, bundleOf("dataText" to getDummyData()))
+        }
+        Espresso.onView(ViewMatchers.withId(R.id.label_ects_value)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.label_ects_value)).check(ViewAssertions.matches(ViewMatchers.withText("5")))
+    }
+
+    private fun getDummyData(): String {
+        return "{\"title\":\"TestTitle\",\"number\":\"Number\",\"semester\":\"Sommer\",\"ects\":5,\"lecturer\":\"Lecturer A\",\"content\":\"description\",\"link\":\"link\"}";
+    }
+}
