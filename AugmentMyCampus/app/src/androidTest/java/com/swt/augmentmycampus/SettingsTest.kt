62fd369ec187c6553763cf7eb3e7fec18962b603
Laurenz Fussenegger <laurenz.fussenegger@gmx.at>
2021-05-26 00:53:32 +0200

    AC-11 implement tests to get login data from backend [FL,LF]

--- AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/SettingsTest.kt
+++ AugmentMyCampus/app/src/androidTest/java/com/swt/augmentmycampus/SettingsTest.kt
@@ -1,6 +1,8 @@
 package com.swt.augmentmycampus
 
 import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
@@ -9,9 +11,22 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jakewharton.espresso.OkHttp3IdlingResource
+import com.swt.augmentmycampus.dependencyInjection.ApplicationModule
+import com.swt.augmentmycampus.dependencyInjection.ConfigurationModule
+import com.swt.augmentmycampus.dependencyInjection.WebserviceConfiguration
+import com.swt.augmentmycampus.network.UserInformationResponse
 import com.swt.augmentmycampus.ui.LocaleManager
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
 import org.hamcrest.CoreMatchers.*
 import org.junit.After
 import org.junit.Before
@@ -22,7 +37,20 @@ import javax.inject.Inject
 
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
+@UninstallModules(ConfigurationModule::class)
 class SettingsTest {
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object FakeConfigurationModule {
+        @Provides
+        fun provideTestWebservice() = WebserviceConfiguration("http://localhost:8080/")
+    }
+
+    private lateinit var mockWebServer: MockWebServer
+
+    var moshi = ApplicationModule.provideJsonSerializer()
+
     @Inject
     lateinit var localeManager: LocaleManager
 
@@ -33,17 +61,27 @@ class SettingsTest {
     var mainActivity: ActivityScenarioRule<MainActivity>
             = ActivityScenarioRule(MainActivity::class.java)
 
+    @Inject
+    lateinit var okHttpClient: OkHttpClient
+
     @Before
     fun setUp() {
         Intents.init()
         hiltRule.inject()
         mainActivity
+        val idlingResource: IdlingResource = OkHttp3IdlingResource.create("OkHttp", okHttpClient)
+        IdlingRegistry.getInstance().register(idlingResource)
+
+        mockWebServer = MockWebServer()
+        mockWebServer.start(port = 8080)
+
         onView(withId(R.id.navigation_settings)).perform(click())
     }
 
     @After
     fun tearDown() {
         Intents.release()
+        mockWebServer.shutdown()
     }
 
     @Test
@@ -81,6 +119,12 @@ class SettingsTest {
 
     @Test
     fun loginFailsWitWrongCredentials() {
+
+        mockWebServer.enqueue(MockResponse().apply {
+            setResponseCode(200)
+            setBody(moshi.adapter(UserInformationResponse::class.java).toJson(null))
+        })
+
         val textToType = "notauser"
         onView(withId(R.id.fragment_login_user)).perform(typeText(textToType))
         closeSoftKeyboard()
@@ -97,7 +141,12 @@ class SettingsTest {
     }
 
     @Test
-    fun loginWithRightCredentials() {
+    fun loginWithRightCredentials1() {
+        mockWebServer.enqueue(MockResponse().apply {
+            setResponseCode(200)
+            setBody(moshi.adapter(UserInformationResponse::class.java).toJson(UserInformationResponse("John", "Smith")))
+        })
+
         val username = "john"
         val password = "123456"
         onView(withId(R.id.fragment_login_user)).perform(typeText(username))
@@ -111,8 +160,27 @@ class SettingsTest {
     }
 
     @Test
+    fun loginWithRightCredentials2() {
+        mockWebServer.enqueue(MockResponse().apply {
+            setResponseCode(200)
+            setBody(moshi.adapter(UserInformationResponse::class.java).toJson(UserInformationResponse("Karl", "Heinz")))
+        })
+
+        val username = "karl"
+        val password = "123456"
+        onView(withId(R.id.fragment_login_user)).perform(typeText(username))
+        closeSoftKeyboard()
+        onView(withId(R.id.fragment_login_password)).perform(typeText(password))
+        closeSoftKeyboard()
+        onView(withId(R.id.fragment_login_submit)).perform(click())
+
+        onView(withText("Karl")).check(matches(isDisplayed()))
+        onView(withText("Heinz")).check(matches(isDisplayed()))
+    }
+
+    @Test
     fun logoutIsWorking() {
-        loginWithRightCredentials()
+        loginWithRightCredentials1()
         val logoutButton = onView(withText("Logout"))
 
         onView(withId(R.id.fragment_settings_logout)).perform(click())
